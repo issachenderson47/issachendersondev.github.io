d30daa2001bb5c66cac7c2dbcc0ad24ca3177b94
issachenderson47 <53947209+issachenderson47@users.noreply.github.com>
2024-01-01 17:02:01 -0500

    1.1.24.7

--- issachendersondev.github.io-main/js/main.js
+++ issachendersondev.github.io-main/js/main.js
@@ -20,6 +20,7 @@ $(window).on('load', function() {
 });
 
 
+
 (function($) {
 
 	/*------------------
@@ -138,3 +139,7 @@ if($().circleProgress){
 
 })(jQuery);
 
+
+<link href="https://assets.calendly.com/assets/external/widget.css" rel="stylesheet">
+<script src="https://assets.calendly.com/assets/external/widget.js" type="text/javascript" async></script>
+<script type="text/javascript">window.onload = function() { Calendly.initBadgeWidget({ url: 'https://calendly.com/issacghenderson/one-on-one-with-issac', text: 'Schedule time with me', color: '#0069ff', textColor: '#ffffff', branding: undefined }); }</script>
